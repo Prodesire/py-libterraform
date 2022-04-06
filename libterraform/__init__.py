2ca8087e958f4a855d204ff7938e8b0b0c820753
Prodesire <Prodesire@yeah.net>
2022-04-06 21:46:14 +0800

    Add test to github action

--- libterraform/__init__.py
+++ libterraform/__init__.py
@@ -2,7 +2,7 @@ import os
 from ctypes import cdll, c_void_p
 from libterraform.common import WINDOWS
 
-__version__ = '0.2.1'
+__version__ = '0.3.0'
 
 root = os.path.dirname(os.path.abspath(__file__))
 _lib_filename = 'libterraform.dll' if WINDOWS else 'libterraform.so'
